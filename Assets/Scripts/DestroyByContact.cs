f7b19038297195eac99b84e56d9b1f95fe29b502
Duncan Bowring <duncan.bowring@saucelabs.com>
2015-06-03 23:39:51 -0700

    Created Hazard prefab (Asteroid) and added VFX
    explosions, movement, and prefab creation.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,9 +2,16 @@
 using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject playerExplosion;
 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Boundary") { return; }
+
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (explosion, other.transform.position, other.transform.rotation);
+		}
 		Destroy(other.gameObject);
 		Destroy (gameObject);
 	}
